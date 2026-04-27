30e6211cbd073a048003a22893e7de9c9d794255
alexandrascola <alexandrakscola@gmail.com>
2026-04-27 14:51:23 -0500

    Added scripts

---
+++ TopDownAdventure/Assets/Scripts/GameManagerSimple.cs
@@ -0,0 +1,103 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.SceneManagement;
+
+//Global Manager for Score and UI
+//Create an empty GameObject aka Game Manager and attach this script
+//Assign score text and message text in the inspector
+
+public class GameManagerSimple : MonoBehaviour
+{
+    //Create singleton loop: create instance of this object and point it at itself
+    public static GameManagerSimple Instance;
+
+    //Define UI elements
+    [Header("UI")]
+    public TextMeshProUGUI scoreText; //optional for this assignment
+    public TextMeshProUGUI messageText;
+    public TextMeshProUGUI healthText; //NEW for "Health: X / Y" display
+
+    //Define global variables
+    private int score = 0;
+    private bool gameOver = false;
+
+
+    //Make a simple singleton loop using void Awake()
+    private void Awake() 
+    {
+        if (Instance == null) Instance = this;
+        else Destroy(gameObject);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        UpdateScoreUI();
+        ShowMessage(""); //empty at start
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //handle win lose and restart
+        if (gameOver && Input.GetKeyDown(KeyCode.R))
+        {
+            Time.timeScale = 1f; //reset time scale in case we paused it
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        }
+    }
+
+    //Void to Add Score
+    public void AddScore(int amount)
+    {
+        if (gameOver) return;
+        score += amount;
+        //call method to update UI
+        UpdateScoreUI();
+
+    }
+
+    //Void to set health
+    public void SetHealth(int current, int max)
+    {
+        if (healthText != null) 
+        {
+            healthText.text = $"Health: {current} / {max}";
+        }
+        
+    }
+
+    //Void to show Message
+    public void ShowMessage(string msg)
+    {
+        if (messageText.text != null) messageText.text = msg;
+    }
+
+
+    //Void to WIN
+    public void Win()
+    {
+        gameOver = true;
+        ShowMessage("You WIN! Press R to Restart");
+        //set time scale to 0 to pause til restart
+        Time.timeScale = 0f;
+    }
+
+    //Void to LOSE
+    public void Lose()
+    {
+        gameOver = true;
+        ShowMessage("GAME OVER. Press R to Restart");
+        //set time scale to 0 to pause til restart
+        Time.timeScale = 0f;
+    }
+
+    //Void to update UI
+    private void UpdateScoreUI()
+    {
+        if (scoreText.text != null) scoreText.text = "Score: " + score;
+    }
+
+}
